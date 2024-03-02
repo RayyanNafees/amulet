23fb08150fc5bdd707120b35ba1279a7534c2e62
RayyanNafees <nafees.rayyan@gmail.com>
2024-03-03 04:42:49 +0530

    App is now a static app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
-import netlify from "@astrojs/netlify";
 import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact() , serviceWorker()],
-  output: "server",
-  adapter: netlify()
 });
