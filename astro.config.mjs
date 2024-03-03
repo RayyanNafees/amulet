97ba08ef2c36372bfd34b68173d23b26bda278c8
RayyanNafees <nafees.rayyan@gmail.com>
2024-03-03 05:35:23 +0530

    Styling setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
 import serviceWorker from "astrojs-service-worker";
+import UnoCSS from 'unocss/astro';
+
+// import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact() , serviceWorker()],
+  integrations: [preact(), serviceWorker(), UnoCSS()],
+  // output: "server",
+  // adapter: netlify()
 });
