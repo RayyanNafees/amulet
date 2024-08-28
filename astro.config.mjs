bcc82e5ad0a4dc3006e4198b390d112fed5afd2e
9node <rayyan@theninthnode.com>
2024-08-29 00:43:59 +0530

    🎉 initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 import astrolace from "@matthiesenxyz/astrolace";
+import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astrolace()],
+	integrations: [
+		astrolace(),
+		preact({ compat: true, devtools: true }),
+		tailwind(),
+	],
 });
